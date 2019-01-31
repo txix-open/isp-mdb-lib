a4980386dadab41b86f9d8d83b195db2c46b4ab9
d1slike <merser1q@gmail.com>
2019-01-31 18:26:54 +0300

    flat diff

--- diff/diff.go
+++ diff/diff.go
@@ -1,7 +1,9 @@
 package diff
 
 import (
+	"github.com/integration-system/bellows"
 	"github.com/integration-system/go-cmp/cmp"
+	"reflect"
 )
 
 type Operation string
@@ -32,4 +34,48 @@ type Delta []*DiffDescriptor
 func EvalDiff(left, right map[string]interface{}, opts ...Option) (bool, Delta) {
 	c := NewDiffCollector(opts...)
 	return cmp.Equal(left, right, c), c.Delta
+}
+
+func FlattenDelta(delta Delta) map[string]*DiffDescriptor {
+	result := make(map[string]*DiffDescriptor, len(delta)*2)
+	for _, desc := range delta {
+		if desc.Operation == Add || desc.Operation == ArrayAdd {
+			rt := reflect.TypeOf(desc.NewValue)
+			switch rt.Kind() {
+			case reflect.Map, reflect.Slice, reflect.Array, reflect.Struct:
+				flattenValue := bellows.Flatten(desc.NewValue)
+				for path, value := range flattenValue {
+					newPath := getNewPath(desc.Path, path, rt.Kind())
+					result[newPath] = &DiffDescriptor{NewValue: value, Path: newPath, Operation: Add}
+				}
+			default:
+				result[desc.Path] = desc
+			}
+		} else if desc.Operation == Delete || desc.Operation == ArrayDelete {
+			rt := reflect.TypeOf(desc.OldValue)
+			switch rt.Kind() {
+			case reflect.Map, reflect.Slice, reflect.Array, reflect.Struct:
+				flattenValue := bellows.Flatten(desc.OldValue)
+				for path, value := range flattenValue {
+					newPath := getNewPath(desc.Path, path, rt.Kind())
+					result[newPath] = &DiffDescriptor{OldValue: value, Path: newPath, Operation: Delete}
+				}
+			default:
+				result[desc.Path] = desc
+			}
+		} else {
+			result[desc.Path] = desc
+		}
+	}
+
+	return result
+}
+
+func getNewPath(base string, path string, kind reflect.Kind) string {
+	switch kind {
+	case reflect.Slice, reflect.Array:
+		return base + path
+	default:
+		return base + "." + path
+	}
 }
