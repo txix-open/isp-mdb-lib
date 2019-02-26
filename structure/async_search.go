4be4d67a5616f3d0afa70985e48d78dcfe190a26
ykomissarov <merser1q@gmail.com>
2019-02-26 11:32:42 +0300

    ref

--- structure/async_search.go
+++ structure/async_search.go
@@ -3,9 +3,18 @@ package structure
 import "github.com/integration-system/isp-mdb-lib/query"
 
 type AsyncSearchRequest struct {
+	PackageSize int
+	Callback    string
+	Protocol    ProtocolVersion
+}
+
+type ExternalAsyncSearchRequest struct {
+	Query map[string]interface{}
+	AsyncSearchRequest
+}
+
+type InternalAsyncSearchRequest struct {
 	Query         query.Term
-	PackageSize   int
-	Callback      string
 	ApplicationId int
-	Protocol      ProtocolVersion
+	AsyncSearchRequest
 }
