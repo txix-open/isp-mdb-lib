9ae5d3dfb616abc12e81b85b127cfc352bb80f88
DeniskaRediska <grj.den@gmail.com>
2021-02-02 19:30:19 +0300

    fix panic

--- stubsV1/erl/erl.go
+++ stubsV1/erl/erl.go
@@ -15,10 +15,57 @@ type PersonsIncoming struct {
 }
 
 type PersonIncoming struct {
-	Citizen           Citizen                 `xml:"citizen,omitempty"`
-	Inn               *StringWithLastModified `xml:"inn,omitempty"`
-	Oms               *StringWithLastModified `xml:"oms,omitempty"`
-	IdentityDocuments *IdentityDocuments      `xml:"identity_documents,omitempty"`
+	ProviderIdentifier string                  `xml:"provider_identifier,omitempty"`
+	Citizen            Citizen                 `xml:"citizen,omitempty"`
+	IdentityDocuments  *IdentityDocuments      `xml:"identity_documents,omitempty"`
+	Inn                *StringWithLastModified `xml:"inn,omitempty"`
+	Oms                *StringWithLastModified `xml:"oms,omitempty"`
+	Death              *Death                  `xml:"death,omitempty"`
+	//Divorce            *DivorceData            `xml:"divorce,omitempty"`
+	CitizenAddresses *CitizenAddresses `xml:"citizen_addresses,omitempty"`
+}
+
+type CitizenAddresses struct {
+	CitizenAddress []CitizenAddress `xml:"citizen_address,omitempty"`
+}
+
+type CitizenAddress struct {
+	AddressType      string                  `xml:"address_type,omitempty"`
+	Address          *Address                `xml:"address,omitempty"`
+	RegistrationDate *StringWithLastModified `xml:"registration_date,omitempty"`
+	DepartureDate    *StringWithLastModified `xml:"departure_date,omitempty"`
+	Lastmodified     string                  `xml:"last_modified,attr"`
+}
+
+type Address struct {
+	BtiIdentity    string `xml:"bti_identity,omitempty"`
+	PostIndex      string `xml:"post_index,omitempty"`
+	SettlementCode string `xml:"settlement_code,omitempty"`
+	StreetCode     string `xml:"street_code,omitempty"`
+	HouseCode      string `xml:"house_code,omitempty"`
+	House          string `xml:"house,omitempty"`
+	Corpus         string `xml:"corpus,omitempty"`
+	Building       string `xml:"building,omitempty"`
+	Flat           string `xml:"flat,omitempty"`
+}
+
+type DivorceData struct {
+	Document        *SimpleDocument         `xml:"document,omitempty"`
+	DivorceDate     *StringWithLastModified `xml:"divorce_date,omitempty"`
+	SourceValueCode string                  `xml:"source_value_code,omitempty"`
+}
+
+type Death struct {
+	Document        *SimpleDocument         `xml:"document,omitempty"`
+	DeathDate       *StringWithLastModified `xml:"death_date,omitempty"`
+	SourceValueCode string                  `xml:"source_value_code,omitempty"`
+}
+
+type SimpleDocument struct {
+	Serial           string `xml:"serial,omitempty"`
+	Number           string `xml:"number,omitempty"`
+	DocIssuedate     string `xml:"doc_issuedate,omitempty"`
+	DocauthorityName string `xml:"docauthority_name,omitempty"`
 }
 
 type Citizen struct {
@@ -37,10 +84,10 @@ type CitizenName struct {
 }
 
 type IdentityDocuments struct {
-	Document []Document `xml:"document,omitempty"`
+	Document []CitizenDocument `xml:"document,omitempty"`
 }
 
-type Document struct {
+type CitizenDocument struct {
 	DoctypePk      string `xml:"doctype_pk,omitempty"`
 	Serial         string `xml:"serial,omitempty"`
 	Number         string `xml:"number,omitempty"`
