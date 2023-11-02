162cebc2102c29ca5b5f1960b4edac54efd79957
gr-dv <grj.den@gmail.com>
2023-11-02 16:14:54 +0300

    update swagger

--- structure/v2_search.go
+++ structure/v2_search.go
@@ -3,14 +3,14 @@ package structure
 import "github.com/integration-system/isp-mdb-lib/query"
 
 type OneOfCondition struct {
-	Logic  *LogicCondition  //логическая условие
-	Binary *BinaryCondition //бинарное условия
+	Logic  *LogicCondition  //логическое условие
+	Binary *BinaryCondition //бинарное условие
 }
 
 type BinaryCondition struct {
 	Field    string         `valid:"required~Required"`                                                                                                                //путь до атрибута
 	Operator query.Operator `valid:"required~Required,in(=|!=|<|>|<=|>=|contains|not contains|starts with|not starts with|ends with|not ends with|exists|not exists)"` //условный оператор
-	Value    interface{}    //(строка, число, булево значение) значение атрибута,обязательно для всех операторов, кроме (exists, not exists)
+	Value    interface{}    //(строка, число, булево значение) значение атрибута, обязательно для всех операторов, кроме (exists, not exists)
 }
 
 type LogicCondition struct {
