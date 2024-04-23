cb993f61c9d66d334f443057f39e5e32e729c4ab
txix <txix@txix.ru>
2024-04-23 10:46:42 +0300

    2024-04-23 10:46:42: merge [master:2ecf375c6834d22e3b3ec127f531c5a4e3687fc5];

---
+++ pdp/consts/validation.go
@@ -0,0 +1,24 @@
+package consts
+
+type ValidationStatus string
+
+const (
+	ValidationStatusPreparingToRequest = ValidationStatus("PREPARING_TO_REQUEST")
+	ValidationStatusPending            = ValidationStatus("PENDING")
+	ValidationStatusProcessing         = ValidationStatus("PROCESSING")
+	ValidationStatusSuccess            = ValidationStatus("SUCCESS")
+	ValidationStatusFailed             = ValidationStatus("FAILED")
+	ValidationStatusCanceled           = ValidationStatus("CANCELED")
+	ValidationStatusExpired            = ValidationStatus("EXPIRED")
+)
+
+type ValidationSystem string
+
+const (
+	ValidationIPEVSystem = ValidationSystem("IPEV")
+	ValidationKSRDSystem = ValidationSystem("KSRD")
+	ValidationEFSPSystem = ValidationSystem("EFSP")
+	ValidationMDMSystem  = ValidationSystem("MDM")
+	ValidationSystem4ME  = ValidationSystem("4ME")
+	ValidationIsvdSystem = ValidationSystem("ISVD")
+)
